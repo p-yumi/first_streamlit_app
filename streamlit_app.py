74f646e5f94e1a4cf612938ad81e595bdb8a4337
p-yumi <87469494+p-yumi@users.noreply.github.com>
2023-04-03 14:36:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kele, Spinnach & Rocket Smoothie')
+streamlit.text('Kele, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
