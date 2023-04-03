862b3806b7e4e20cab65f57e9b6ac86f29c51b58
p-yumi <87469494+p-yumi@users.noreply.github.com>
2023-04-03 15:15:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
---#display the table on the page
---streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index)) 
+#--#display the table on the page
+#--streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index)) 
 # Let's put a pick list here so they can pick the frute tehey want to include
 streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index),['Avocado','Strawberries']) 
 
